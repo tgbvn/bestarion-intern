ba7ce479345720c9ffe7863206c4a37d09ae3c7c
VuVT <truongvuuit@gmail.com>
2021-11-13 10:57:40 +0700

    Review

--- 8-11-2021/array_min.rb
+++ 8-11-2021/array_min.rb
@@ -1,5 +1,5 @@
 
-arr = Array.new
+arr = Array.new # biến chứa nhiều phần tử nên là số nhiều
 puts "Nhap vao mang so tu nhien"
 print "Nhap so dau tien: "
 while (temp = gets.chomp) != "-1" do
@@ -11,3 +11,6 @@ while (temp = gets.chomp) != "-1" do
   print "Nhap so tiep theo: "
 end
 puts("So nho nhat trong mang: #{arr.sort.first}" )
+
+# A rất ấn tượng với chương trình của e. 
+# E cần thêm lable khi commit code.
