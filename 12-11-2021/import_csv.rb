9e3f2bfa3b442376c77bb07aff6ddb8b81e05682
VuVT <truongvuuit@gmail.com>
2021-11-19 15:02:36 +0700

    Review ex3

--- 12-11-2021/import_csv.rb
+++ 12-11-2021/import_csv.rb
@@ -37,3 +37,8 @@ puts User.count
 #500000
 puts User.find_by(email: 'nguyenvana500000@gmail.com').inspect
 ##<User email: "nguyenvana500000@gmail.com", name: "Nguyen Van A500000", phone: "0123456789", address: "Ho Chi Minh city", day_of_birth: "2021-01-01", profile: "TV 100\", Some special charactor: \\ / ' $ ~ & @ # (...">
+
+# Chương trình của e viết tốt, biết chia file, hàm, và thời gian chạy cũng rất ấn tượng
+# Phần thông tin kết nối e có thể thêm 1 file nữa để bảo mật
+# Em đang fetch lên RAM toàn bộ nội dung của file rồi insert 1 lần xuống db, thì có thể thiếu RAM với những data lớn hơn.
+# Nên a nghĩ là e có thể chia theo batch để xử lý và insert vào db, vd: đọc 10k lines rồi insert xuống 1 lần.
