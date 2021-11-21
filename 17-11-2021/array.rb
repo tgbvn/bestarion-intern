eecb056b4d1f2e35f2bdafe52d3a8f5a9ae5e772
tgbvn <tgbaovn@gmail.com>
2021-11-21 20:21:46 +0700

    [Ex5] a new file

---
+++ 17-11-2021/array.rb
@@ -0,0 +1,37 @@
+#Tim mang con tang dan
+
+def only_integer?(str)
+  str.match(/\A[+]?\d+\z/)
+end
+
+def arr_create
+  arr_result = Array.new
+  input = String.new
+  loop do
+    print "Nhap vao so luong phan tu: "
+    input = gets.chomp
+    break if only_integer?(input)
+  end
+  input.to_i.times { arr_result.push(rand(1..9)) }
+  arr_result
+end
+
+def arr_child_asc_build(arr_parent)
+  arr_child = Array.new
+  i = -1
+  while i < (arr_parent.length - 1)
+    arr_asc = Array.new
+    loop do
+      i += 1
+      arr_asc.push(arr_parent[i])
+      break if arr_parent[i+1].nil? || (arr_parent[i] > arr_parent[i+1])
+    end
+    arr_child.push(arr_asc)
+  end
+  arr_child
+end
+
+arr_parent = arr_create
+puts "Mang da tao: " + arr_parent.to_s
+puts "Mang con tang dan: " + arr_child_asc_build(arr_parent).to_s
+
