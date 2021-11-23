bb28f226605e09eaca7a1a1ebaf789a3bc6d3f1e
tgbvn <40329096+tgbvn@users.noreply.github.com>
2021-11-23 22:35:23 +0700

    [Ex6] update

--- 22-11-2021/target_sum.rb
+++ 22-11-2021/target_sum.rb
@@ -1,15 +1,15 @@
 #Tim hai phan tu co tong theo yeu cau
 
-TARGET_SUM = 2
+TARGET_SUM = 10
 ARR_NUMBERS = [3, 5, -4, 8, 11, 1, -1, 6]
 
-def find_two_elements(arr_input,target)
+def find_two_elements(arr_input,target_sum)
   arr_result = Array.new
   arr_length = arr_input.length
   base_index = 0
   for step in 0..(arr_length*arr_length - 1)
     mirror_index = step % arr_input.length
-    if (arr_input[base_index] + arr_input[mirror_index]) == target && arr_input[base_index] != arr_input[mirror_index]
+    if (arr_input[base_index] + arr_input[mirror_index]) == target_sum && arr_input[base_index] != arr_input[mirror_index]
       arr_result = [arr_input[base_index],arr_input[mirror_index]]
       break
     end
@@ -21,6 +21,3 @@ def find_two_elements(arr_input,target)
 end
 
 puts "Ket qua: " + find_two_elements(ARR_NUMBERS,TARGET_SUM).to_s
-
-
-
